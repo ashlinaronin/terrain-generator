32ba15c4b23e1c4803e590b6f02f3889389ea7bf
ashlinaronin <ashlinaronin@gmail.com>
2020-09-16 21:44:33 -0700

    Build out random height map generation and reset button

---
+++ Assets/CustomTerrain.cs
@@ -0,0 +1,92 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+[ExecuteInEditMode]
+public class CustomTerrain : MonoBehaviour {
+
+	public Vector2 randomHeightRange = new Vector2(0, 0.1f);
+	public Terrain terrain;
+	public TerrainData terrainData;
+
+	public void RandomTerrain()
+	{
+		float[,] heightMap = terrainData.GetHeights(0, 0, terrainData.heightmapWidth, terrainData.heightmapHeight);
+		for (int x = 0; x < terrainData.heightmapWidth; x++)
+		{
+			for (int y = 0; y < terrainData.heightmapHeight; y++)
+			{
+				heightMap[x, y] += UnityEngine.Random.Range(randomHeightRange.x, randomHeightRange.y);
+			}
+			terrainData.SetHeights(0, 0, heightMap);
+		}
+	}
+
+	public void ResetTerrain()
+	{
+		float[,] heightMap = new float[terrainData.heightmapWidth, terrainData.heightmapHeight];
+		for (int x = 0; x < terrainData.heightmapWidth; x++)
+		{
+			for (int y = 0; y < terrainData.heightmapHeight; y++)
+			{
+				heightMap[x,y] = 0;
+			}
+		}
+		terrainData.SetHeights(0, 0, heightMap);
+	}
+
+	void OnEnable()
+	{
+		Debug.Log("Initializing Terrain Data");
+		terrain = this.GetComponent<Terrain>();
+		terrainData = terrain.terrainData;
+	}
+
+	void Awake()
+	{
+		SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+		SerializedProperty tagsProp = tagManager.FindProperty("tags");
+
+		AddTag(tagsProp, "Terrain");
+		AddTag(tagsProp, "Cloud");
+		AddTag(tagsProp, "Shore");
+
+		// apply tag changes to tag database
+		tagManager.ApplyModifiedProperties();
+
+		// tag this object
+		this.gameObject.tag = "Terrain";
+	}
+
+	void AddTag(SerializedProperty tagsProp, string newTag)
+	{
+		bool found = false;
+
+		// ensure the tag doesn't already exist
+		for (int i = 0; i< tagsProp.arraySize; i++)
+		{
+			SerializedProperty t = tagsProp.GetArrayElementAtIndex(i);
+			if (t.stringValue.Equals(newTag)) { found = true; break;}
+		}
+
+		// add your new tag
+		if (!found)
+		{
+			tagsProp.InsertArrayElementAtIndex(0);
+			SerializedProperty newTagProp = tagsProp.GetArrayElementAtIndex(0);
+			newTagProp.stringValue = newTag;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Editor/CustomTerrainEditor.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEditor;
+using EditorGUITable;
+
+[CustomEditor(typeof(CustomTerrain))]
+[CanEditMultipleObjects]
+public class CustomTerrainEditor : Editor {
+
+	// properties ---------------------
+	SerializedProperty randomHeightRange;
+
+	// fold outs ----------------------
+	bool showRandom = false;
+
+	void OnEnable()
+	{
+		randomHeightRange = serializedObject.FindProperty("randomHeightRange");
+	}
+
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update();
+
+		CustomTerrain terrain = (CustomTerrain)target;
+
+		showRandom = EditorGUILayout.Foldout(showRandom, "Random");
+		if (showRandom)
+		{
+			EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+			GUILayout.Label("Set Heights Between Random Values", EditorStyles.boldLabel);
+			EditorGUILayout.PropertyField(randomHeightRange);
+			if (GUILayout.Button("Random Heights"))
+			{
+				terrain.RandomTerrain();
+			}
+		}
+
+		if (GUILayout.Button("Reset Terrain"))
+		{
+			terrain.ResetTerrain();
+		}
+
+		serializedObject.ApplyModifiedProperties();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
